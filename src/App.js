10f3dd0a601de767c384e39b713d1ab567cb29e5
Alexey Belov <slesar83@gmail.com>
2023-04-26 16:58:26 +0300

    Add React components

--- src/App.js
+++ src/App.js
@@ -1,9 +1,20 @@
+import React, { useState } from "react";
+import Counter from "./components/Counter";
+import ClassCounter from "./components/ClassCounter";
+import "./styles/App.css";
+import PostItem from "./components/PostItem";
+import PostList from "./components/PostList";
+
 function App() {
+  const [posts, setPosts] = useState([
+    { id: 1, title: "Javascript 1", body: "Description 1" },
+    { id: 2, title: "Javascript 2", body: "Description 2" },
+    { id: 3, title: "Javascript 3", body: "Description 3" },
+  ]);
+
   return (
     <div className="App">
-      <header className="App-header">
-        <p>Learn React!!!</p>
-      </header>
+      <PostList posts={posts} title="Список постов 1"></PostList>
     </div>
   );
 }
