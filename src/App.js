e615d131a6939e275692e6b04c9cad4657a3d44d
Alexey Belov <slesar83@gmail.com>
2023-04-27 13:35:24 +0300

    Add double binding

--- src/App.js
+++ src/App.js
@@ -4,6 +4,8 @@ import ClassCounter from "./components/ClassCounter";
 import "./styles/App.css";
 import PostItem from "./components/PostItem";
 import PostList from "./components/PostList";
+import MyButton from "./components/UI/button/MyButton";
+import MyInput from "./components/UI/input/MyInput";
 
 function App() {
   const [posts, setPosts] = useState([
@@ -12,9 +14,27 @@ function App() {
     { id: 3, title: "Javascript 3", body: "Description 3" },
   ]);
 
+  const [title, setTitle] = useState("ewdew");
+
+  const addNewPost = (e) => {
+    e.preventDefault();
+    console.log(title);
+  };
+
   return (
     <div className="App">
-      <PostList posts={posts} title="Список постов 1"></PostList>
+      <form>
+        {/* Управляемы компонент */}
+        <MyInput
+          type="text"
+          placeholder="Название поста"
+          value={title}
+          onChange={(e) => setTitle(e.target.value)}
+        />
+        <MyInput type="text" placeholder="Описание поста" />
+        <MyButton onClick={addNewPost}>Создать пост</MyButton>
+      </form>
+      <PostList posts={posts} title="Посты про JS"></PostList>
     </div>
   );
 }
